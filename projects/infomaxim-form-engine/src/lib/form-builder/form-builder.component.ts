d8804c450391b62ef826eb9bc912d74dc569ba1e
andy008 <andy@infomaxim.com>
2021-05-12 20:32:41 +1000

    feat: render columns, section and fields

--- projects/infomaxim-form-engine/src/lib/form-builder/form-builder.component.ts
+++ projects/infomaxim-form-engine/src/lib/form-builder/form-builder.component.ts
@@ -8,7 +8,7 @@ import { NgForm, FormControl, FormGroup, ValidationErrors, Validators } from '@a
 })
 export class FormBuilderComponent implements OnInit {
 
-  @Input() formConfig: any[] = [];
+  @Input() formConfig: any;
   public formTitle: String = '';
   public fields: any[] = [];
   public form: FormGroup;
@@ -20,40 +20,50 @@ export class FormBuilderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let fieldsCtrls = {};
     this.formTitle = this.formConfig['title'];
-    this.fields = this.formConfig['fields'];
+
+    for (const key1 in this.formConfig['columns']) {
+      let column = this.formConfig.columns[key1];
+      for (const key2 in column['sections']) {    
+        let section = column.sections[key2];   
+        for (const key3 in section['fields']) {
+          let field = section.fields[key3];
+          console.log(JSON.stringify(field));
+          if (field?.required) {
+            let validators = [Validators.required];
+            if (field?.type === 'email') {
+              validators.push(Validators.email);
+            }
+            if (field?.minLength) {
+              validators.push(Validators.minLength(field.minLength));
+            }
+            if (field?.maxLength) {
+              validators.push(Validators.maxLength(field.maxLength));
+            }
+            if (field?.pattern) {
+              validators.push(Validators.pattern(field.pattern));
+            }
+            fieldsCtrls[field.name] = new FormControl(field.value || '', validators);
+          } else {
+            fieldsCtrls[field.name] = new FormControl(field.value || '');
+          };
+        };        
+      };
+    };
+           
     if (this.formConfig && this.formConfig['controls'] && this.formConfig['controls']['saveButton']) {
       this.saveButton = this.formConfig['controls']['saveButton'];
     }
     if (this.formConfig && this.formConfig['controls'] && this.formConfig['controls']['resetButton']) {
       this.resetButton = this.formConfig['controls']['resetButton'];
     }
-    let fieldsCtrls = {};
-    for (let field of this.fields) {
-      if (field?.required) {
-        let validators = [Validators.required];
-        if (field?.type === 'email') {
-          validators.push(Validators.email);
-        }
-        if (field?.minLength) {
-          validators.push(Validators.minLength(field.minLength));
-        }
-        if (field?.maxLength) {
-          validators.push(Validators.maxLength(field.maxLength));
-        }
-        if (field?.pattern) {
-          validators.push(Validators.pattern(field.pattern));
-        }
-        fieldsCtrls[field.name] = new FormControl(field.value || '', validators);
-      } else {
-        fieldsCtrls[field.name] = new FormControl(field.value || '');
-      }
-    }
+
     this.form = new FormGroup(fieldsCtrls);
   }
 
   submit() {
-    if (this.getFormValidationErrors(this.form).length) {
+    if (this.getFormValidationErrors(this.form)) {
       this.form.markAllAsTouched();
     } else {
       this.sendFormData.emit(this.form.value);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,97 +15,95 @@ export class AppComponent {
     },
     columns: [
       {
-        class: 'col-12 col-lg-6',
+        class: 'col-12 col-lg-6 pb-4 pb-lg-0',
         sections: [
           {
-          class: 'ml-1',
-          fields: [
-            {
-              type: 'heading',
-              htag: 'h4',
-              class: 'contact-us__header card-title',
-              content: 'Instructions'
-            },   
-            {
-              type: 'content',
-              class: 'contact-us__address',
-              content:'<p>After submitting this request you will receive a notification email.<\/p><p>Please remove the SIM card and the memory card if applicable and include the Proof of Purchase. This can be obtained from your history page. <\/p><p>To check your mobile phone IMEI, please enter the following sequence in your mobile phone *#06#. The IMEI is a 15 digit serial number. <\/p><p>The Proof of Purchase will also have the IMEI recorded.<\/p><p>Our authorised repairer, MHD Services will contact you, organise a courier for pick up from your premises and email the consignment note that needs to be printed and attached to the package containing the device. Please ensure that the device is securely packaged and ready at the nominated location for collection.<\/p>'
-            }
-          ]
+            class: 'mr-1',
+            fields: [
+              {
+                type: 'heading',
+                htag: 'h4',
+                class: 'contact-us__header card-title',
+                content: 'Instructions'
+              },   
+              {
+                type: 'content',
+                class: 'contact-us__address',
+                content:'<p>After submitting this request you will receive a notification email.<\/p><p>Please remove the SIM card and the memory card if applicable and include the Proof of Purchase. This can be obtained from your history page. <\/p><p>To check your mobile phone IMEI, please enter the following sequence in your mobile phone *#06#. The IMEI is a 15 digit serial number. <\/p><p>The Proof of Purchase will also have the IMEI recorded.<\/p><p>Our authorised repairer, MHD Services will contact you, organise a courier for pick up from your premises and email the consignment note that needs to be printed and attached to the package containing the device. Please ensure that the device is securely packaged and ready at the nominated location for collection.<\/p>'
+              }
+            ]
           },
           {
-            render: true,
             class: 'ml-1',            
             fields: [                    
-            {
-              type: 'text',
-              name: 'fullName',
-              label: 'Full Name',
-              value: '',
-              required: true,
-              validationMessage: 'Full Name is required.',
-              multiline: false,
-              placeholder: 'Full Name'
-            },
-            {
-              type: 'email',
-              name: 'email',
-              label: 'Email Address',
-              value: '',
-              required: true,
-              validationMessage: 'Email Address is required.',
-              placeholder: 'Email Address'
-            },
-            {
-              type: 'text',
-              name: 'description',
-              label: 'Description',
-              value: '',
-              required: false,
-              multiline: true,
-              lines: 5,
-              placeholder: 'Description'
-            },
-            {
-              type: 'radio',
-              name: 'gender',
-              label: 'Gender',
-              required: true,
-              style: 'row',
-              value: '',
-              options: [
-                { key: 'male', label: 'Male' },
-                { key: 'female', label: 'Female' },
-                { key: 'other', label: 'Other' }
-              ],
-              validationMessage: 'Select Gender.'
-            },     
-            {
-              type: 'date',
-              name: 'birthdate',
-              label: 'Birth Date',
-              // value: '2018-07-22',
-              // min: '2018-01-01',
-              // max: '2018-12-31',
-              required: true,
-              validationMessage: 'Please Select Birth Date.',
-            },
-            {
-              type: 'time',
-              name: 'birthtime',
-              label: 'Birth Time',
-              // value: '06:35',
-              required: true,
-              validationMessage: 'Please Select Birth Time.',
-            }
-          ]
-        }]
+              {
+                type: 'text',
+                name: 'fullName',
+                label: 'Full Name',
+                value: '',
+                required: true,
+                validationMessage: 'Full Name is required.',
+                multiline: false,
+                placeholder: 'Full Name'
+              },
+              {
+                type: 'email',
+                name: 'email',
+                label: 'Email Address',
+                value: '',
+                required: true,
+                validationMessage: 'Email Address is required.',
+                placeholder: 'Email Address'
+              },
+              {
+                type: 'text',
+                name: 'description',
+                label: 'Description',
+                value: '',
+                required: false,
+                multiline: true,
+                lines: 5,
+                placeholder: 'Description'
+              },
+              {
+                type: 'radio',
+                name: 'gender',
+                label: 'Gender',
+                required: true,
+                style: 'row',
+                value: '',
+                options: [
+                  { key: 'male', label: 'Male' },
+                  { key: 'female', label: 'Female' },
+                  { key: 'other', label: 'Other' }
+                ],
+                validationMessage: 'Select Gender.'
+              },     
+              {
+                type: 'date',
+                name: 'birthdate',
+                label: 'Birth Date',
+                // value: '2018-07-22',
+                // min: '2018-01-01',
+                // max: '2018-12-31',
+                required: true,
+                validationMessage: 'Please Select Birth Date.',
+              },
+              {
+                type: 'time',
+                name: 'birthtime',
+                label: 'Birth Time',
+                // value: '06:35',
+                required: true,
+                validationMessage: 'Please Select Birth Time.',
+              }
+            ]
+          }
+        ]
       },
       {
-        type: 'column',
         class: 'col-12 col-lg-6',
-        sections: {
-          render: true,
+        sections: [{
           class: 'ml-1',
           fields: [       
             {
@@ -187,7 +185,7 @@ export class AppComponent {
               validationMessage: 'Please Select Social Account.'
             }
           ]
-        }
+        }]
       }
     ]
   };
