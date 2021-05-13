bb523d8ef2c29a2234a318390982f3150e80e435
andy008 <andy@infomaxim.com>
2021-05-13 10:35:50 +1000

    fix: naming and browser module

--- projects/infomaxim-form-engine/src/lib/infomaxim-form-engine.module.ts
+++ projects/infomaxim-form-engine/src/lib/infomaxim-form-engine.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+//import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common'; 
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -39,7 +39,7 @@ import { ContentComponent } from './fields/content/content.component';
     ContentComponent
   ],
   imports: [
-    BrowserModule,
+    //BrowserModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-//import { InfomaximFormBuilderModule } from '@andyf008/infomaxim-form-engine';
+//import { InfomaximFormBuilderModule } from '@andyf008/infomaxim6-form-engine';
 import { InfomaximFormBuilderModule } from '../../projects/infomaxim-form-engine/src/lib/infomaxim-form-engine.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
