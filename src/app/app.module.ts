a51aa56a80f3efaa58ce574319f139cb78a2ec34
andy008 <andy@infomaxim.com>
2022-06-15 15:47:49 +1000

    upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 //import { InfomaximFormBuilderModule } from '@andyf008/infomaxim6-form-engine';
 import { InfomaximFormBuilderModule } from '../../projects/infomaxim-form-engine/src/lib/infomaxim-form-engine.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
+    //NgbModule,
     InfomaximFormBuilderModule,
     AppRoutingModule,
     BrowserAnimationsModule
